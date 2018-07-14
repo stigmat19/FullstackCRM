37841279ddc005ed56a039f6ab73db22fbd06c61
Stig1988 <En198806044815162342>
2018-07-15 00:30:28 +0300

    first commit block 3 lesson 4

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://Stigmat1988:En198806044815162342@ds139585.mlab.com:39585/fullstack',
+  jwt: 'dev-jwt'
+};
---
+++ models/Users.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const usersSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('users', usersSchema);
