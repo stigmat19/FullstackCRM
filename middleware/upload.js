1a855ae3388bc29f8142cc568b1b8f1d8d6233d2
Stig1988 <En198806044815162342>
2018-07-17 23:32:16 +0300

    first commit block 4 lesson 4 Конфигурация загрузки файлов

---
+++ middleware/upload.js
@@ -0,0 +1,29 @@
+const multer = require('multer');
+const moment = require('moment');
+
+const storage = multer.diskStorage({
+  destination(req, file, cb){
+    cb(null, 'uploads/')
+  },
+  filename(req, file, cb){
+    const date = moment().format('DDMMYYYY-HHmmss_SSS');
+    cb(null, `${date}-${file.originalname}`);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if(file.mimeType === 'image/png' || file.mimeType === 'image/jpeg'){
+    cb(null, true)
+  }
+  else{
+    cb(null, false)
+  }
+};
+
+const limilts = {
+  fileSize: 1024*1024*5
+};
+
+
+
+module.exports = multer({storage, fileFilter, limilts});
